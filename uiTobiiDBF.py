d9f72f878bb65432032a63c3f8b0270781ac131b
mlacayoemery <mlacayoemery@gmail.com>
2009-03-02 08:31:32 +0000

    Began generating pydoc documentation.

--- uiTobiiDBF.py
+++ uiTobiiDBF.py
@@ -1,3 +1,16 @@
+"""The user interface to the class tobiiParse.
+
+From ArcGIS use provided toolbox, or add script with parameter for input file, output dbf Table, and boolean.
+Calls tobiiParse with parameters, use boolean to decide to dynamically type.
+"""
+__author__ = "Martin Lacayo-Emery <positrons@gmail.com>"
+__date__ = "02 March 2009"
+
+__version__ = "$Revision: 1 $"
+__credits__ = """Arzu \xc7\xf6ltekin, project collaborator
+Sara Fabrikant, project collaborator
+Fulbright Program, funding agency
+"""
 import sys
 import lib.pop.tobiiParse
 
