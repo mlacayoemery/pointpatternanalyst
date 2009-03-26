70362a19f3ac616c9dafbc36c5238739921c2251
mlacayoemery <mlacayoemery@gmail.com>
2009-03-26 11:01:56 +0000

    Fixed geometry creation.

--- uiDBFshape.py
+++ uiDBFshape.py
@@ -16,7 +16,7 @@ Fulbright Program, funding agency
 """
 
 import sys
-import lib.pop.tableToShape
+import lib.shp.tableToShape
 
 if __name__ == "__main__":
     inName=sys.argv[1]
@@ -24,10 +24,10 @@ if __name__ == "__main__":
     yField=sys.argv[3]
     outName=sys.argv[4]
     quadrant=sys.argv[5]
-    shape=sys.argv[6]
+    shape=lib.shp.tableToShape.shapefile.shapeTypes[sys.argv[6]]
     if sys.argv[7]=="true":
         changeTypes=True
     else:
         changeTypes=False
         
-    lib.pop.tableToShape.TableToShape(inName,xField,yField,outName,quadrant,shape,changeTypes)
+    lib.shp.tableToShape.TableToShape(inName,xField,yField,outName,quadrant,shape,changeTypes)
