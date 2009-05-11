c4bef47e4efe3760c0d566196e71ad0512cbaa6d
mlacayoemery <mlacayoemery@gmail.com>
2009-05-11 08:38:14 +0000

    Changed design of toolbox. Added style sheet. Consolidated Tobii and Animeye parsing.

---
+++ uiToXbase.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__=="__main__":
+    inName=sys.argv[1]
+    conversionType=sys.argv[2]
+    outName=sys.argv[3]
+    if sys.argv[4]=="true":
+        dynamicSpecs=True
+    else:
+        dynamicSpecs=False
+    if convertsionType=="Tobii":
+        import lib.pop.tobiiDBF
+        lib.pop.tobiiDBF.tobiiParse(inName,outName,dynamicSpecs)
+    elif coversionType=="SensoMotoric":
+        import lib.pop.animeyeDBF
+        lib.pop.animeyeDBF.animeyeParse(inName,outName,dynamicSpecs)
+    else:
+        raise ValueError, conversionType + " is not a defined format."
