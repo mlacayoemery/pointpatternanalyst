bb5cfdfebf1a26b594b61adf5f33d7367d809343
mlacayoemery <mlacayoemery@gmail.com>
2009-05-19 09:02:52 +0000

    Modified Tobii parsing to support all headers.

--- lib/pop/tobiiDBF.py
+++ lib/pop/tobiiDBF.py
@@ -3,15 +3,17 @@ import sys
 sys.path.append(sys.argv[0][:sys.argv[0].rfind("\\")+1]+"\\lib\\shp")
 import databasefile
 
-headerLength=24
-
 def tobiiParse(inName,outName,dynamicSpecs=False):
     tobiiParseFile(open(inName,'r'),open(outName,'wb'),dynamicSpecs)
 
 def tobiiParseFile(inFile,outFile,dynamicSpecs=False):
     #read in file, remove header, strip and split lines
-    lines=[l.replace("\t\n","\n").split("\t") for l in inFile.readlines()[headerLength:]]
+    lines=inFile.readlines()
     inFile.close()
+    lines.reverse()
+    lines=lines[:lines.index("\n")]
+    lines.reverse()
+    lines=[l.replace("\t\n","").split("\t") for l in lines]
     header=lines.pop(0)
     
     d=databasefile.DatabaseFile(header,None,lines)
