9b15fe9bd439ed726fb6d388bd5d58b7218079f2
mlacayoemery <mlacayoemery@gmail.com>
2009-06-03 15:33:15 +0000

    Added data for sequences. Modified append to handle file headers.

--- append.py
+++ append.py
@@ -7,14 +7,31 @@ if __name__=="__main__":
     
     inFolder=sys.argv[1]
     outName=sys.argv[2]
+    if sys.argv[3]=="true":
+        header=True
+    else:
+        header=False
+        
     outFile=open(outName,"w")
-    
+
+    finalHeader=""
+    if header:
+        for name in os.listdir(inFolder):
+            inName=inFolder+"\\"+name
+            inFile=open(inName)
+            tempHeader=inFile.readline()
+            if len(tempHeader)>len(finalHeader):
+                finalHeader=tempHeader
+            inFile.close()
+        outFile.write(finalHeader)
+        
     for name in os.listdir(inFolder):
         inName=inFolder+"\\"+name
         gp.AddMessage("Processing "+inName)
         inFile=open(inName)
-        outFile.write(inFile.read()+"\n")
+        if header:
+            inFile.readline()
+        outFile.write(inFile.read().strip()+"\n")
         inFile.close()
         
     outFile.close()
- 
---
+++ lib/pop/randomSeq.py
@@ -0,0 +1,43 @@
+import random
+import string
+
+def randomSequence(idLength,alphabetSize,wordLength,sequenceLength,totalSequences,outName):
+    startChars=string.ascii_uppercase[:alphabetSize]
+    endChars=string.ascii_lowercase[:alphabetSize]
+    outFile=open(outName,'w')
+
+    #create all sequences
+    for i in range(totalSequences):
+        #create a Id
+        outFile.write("> ")
+        for j in range(idLength):
+            outFile.write(string.ascii_lowercase[random.randint(0,25)])
+        outFile.write("\n")
+
+        #create a sequence
+        line=0
+        for k in range(sequenceLength):
+            #write first letter of word
+            if line==72:
+                outFile.write("\n")
+                line=0            
+            outFile.write(startChars[random.randint(0,alphabetSize-1)])
+            line=line+1
+            #write rest of letters of word
+            for l in range(wordLength-1):
+                if line==72:
+                    outFile.write("\n")
+                    line=0
+                outFile.write(endChars[random.randint(0,alphabetSize-1)])
+                line=line+1
+        outFile.write("\n")
+    outFile.close()
+                
+if __name__=="__main__":
+    idLength=10
+    alphabetSize=26
+    wordLength=2
+    sequenceLength=20
+    totalSequences=10
+    outName="E:\\work\\randomseq2.txt"
+    randomSequence(idLength,alphabetSize,wordLength,sequenceLength,totalSequences,outName)
