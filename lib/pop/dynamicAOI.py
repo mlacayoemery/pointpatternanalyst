9001b198d83e22da65516f9a5b87f4c7c49749d7
mlacayoemery <mlacayoemery@gmail.com>
2009-12-15 06:20:19 +0000

    Worked on dynamic AOI detection.

--- lib/pop/dynamicAOI.py
+++ lib/pop/dynamicAOI.py
@@ -7,19 +7,23 @@ import Image
 ##    print inName
 
 base="E:\\IJGIS\\BP27Cg\\"
-report=open(base+"report.txt","w")
+report=open("E:\\IJGIS\\report.txt","w")
 windowStatus=-1
-for i in range(0,2051):
-    name=("0"*(4-len(str(i))))+str(i)+".jpeg"
+for i in range(1,2052):
+    if i%100==1:
+        print i
+    name=("0"*(4-len(str(i))))+str(i)+".jpg"
     inName=base+name
     im = Image.open(inName)
-    if im.getpixel((1482,854))==(227, 254, 255) and (windowStatus==0 or windowStatus == -1):
-        report.write(name + " Window open.\n")
-        windowStatus=1
-    elif im.getpixel((1482,1098))==(235, 251, 255) and (windowStatus==1 or windowStatus == -1):
-        report.write(name + " Window closed.\n")
-        windowStatus=0
-    elif windowStatus==0 or windowStatus==1:
+    if im.getpixel((1482,854))==(227, 254, 255):
+        if windowStatus!=1:
+            report.write(name + " Window open.\n")
+            windowStatus=1
+    elif im.getpixel((1482,1098))==(235, 251, 255):
+        if windowStatus!=0:
+            report.write(name + " Window closed.\n")
+            windowStatus=0
+    elif windowStatus!=-1:
         report.write(name + " Window missing.\n")
         windowStatus=-1
 report.close()
